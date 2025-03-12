bc2ae4874b9e198183d92f735135613144b4a500
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-03-12 22:44:14 +0500

    Recharts added

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,3 +1,4 @@
+import CountChart from "@/components/CountChart"
 import UserCart from "@/components/UserCard"
 
 
@@ -5,11 +6,24 @@ function AdminPage() {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
 {/* LEFT */}
+<div className="w-full lg:w-2/3 flex flex-col gap-8">
+
 <div className=" flex gap-4 justify-between flex-wrap ">
 <UserCart type="student"/>
 <UserCart type="teacher"/>
 <UserCart type="parent"/>
 <UserCart type="staff"/>
+</div>
+{/* MIDDLE CHARTS */}
+<div className="flex gap-4 flex-col lg:flex-row">
+{/* Count Chart */}
+<div className="w-full lg:w-1/3 h-[450px]" >
+<CountChart/>
+
+</div>
+{/* Attendence chart */}
+<div className="w-full lg:w-2/3 h-[450px]"></div>
+</div>
 </div>
 {/* RIGHT */}
 <div className="w-full lg:w-1/3 ">right</div>
