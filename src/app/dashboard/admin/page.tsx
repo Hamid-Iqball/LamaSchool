457272a8f49578fbc8d1192c1f3664fa1afba5c3
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-03-13 15:50:25 +0500

    added calender with Reactr calender

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -2,39 +2,41 @@ import CountChart from "@/components/CountChart"
 import UserCart from "@/components/UserCard"
 import AttendenceChart from "@/components/AttendenceChart"
 import FinanceChart from "@/components/FinanceChart"
+import EventCalender from "@/components/EventCalender"
 
 function AdminPage() {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
-{/* LEFT */}
-<div className="w-full lg:w-2/3 flex flex-col gap-8">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
 
-<div className=" flex gap-4 justify-between flex-wrap ">
-<UserCart type="student"/>
-<UserCart type="teacher"/>
-<UserCart type="parent"/>
-<UserCart type="staff"/>
-</div>
-{/* MIDDLE CHARTS */}
-<div className="flex gap-4 flex-col lg:flex-row">
-{/* Count Chart */}
-<div className="w-full lg:w-1/3 h-[450px]" >
-<CountChart/>
+      <div className=" flex gap-4 justify-between flex-wrap ">
+      <UserCart type="student"/>
+      <UserCart type="teacher"/>
+      <UserCart type="parent"/>
+      <UserCart type="staff"/>
+      </div>
+      {/* MIDDLE CHARTS */}
+      <div className="flex gap-4 flex-col lg:flex-row">
+      {/* Count Chart */}
+      <div className="w-full lg:w-1/3 h-[450px]" >
+      <CountChart/>
 
-</div>
-{/* Attendence chart */}
-<div className="w-full lg:w-2/3 h-[450px]">
-<AttendenceChart />
-</div>
-</div>
-{/* BottomChart */}
-<div className="w-full h-[500px]">
-<FinanceChart />
-</div>
-</div>
-{/* RIGHT */}
-<div className="w-full lg:w-1/3 ">right</div>
-    </div>
+      </div>
+      {/* Attendence chart */}
+      <div className="w-full lg:w-2/3 h-[450px]">
+      <AttendenceChart />
+      </div>
+      </div>
+      {/* BottomChart */}
+      <div className="w-full h-[500px]">
+      <FinanceChart />
+      </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8 ">
+      <EventCalender/></div>
+          </div>
   )
 }
 
