ac62ce13c1c2426522924991ff7d689f4a072cfe
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-03-13 16:33:30 +0500

    Events completed

--- src/components/EventCalender.tsx
+++ src/components/EventCalender.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Image from "next/image";
 import { useState } from "react";
 import Calendar from "react-calendar";
 import 'react-calendar/dist/Calendar.css';
@@ -7,11 +8,51 @@ import 'react-calendar/dist/Calendar.css';
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
+//TEMPORARY
+const events =[
+  {
+    id:1,
+    title:"Lorem ispm dolor",
+    time:"12:00  pm - 2:00 PM",
+    description:"Lorem ispm dolor sit ament , dirt ainitdngn"
+  },
+  {
+    id:2,
+    title:"Lorem ispm dolor",
+    time:"12:00  pm - 2:00 PM",
+    description:"Lorem ispm dolor sit ament , dirt ainitdngn"
+  },
+  {
+    id:3,
+    title:"Lorem ispm dolor",
+    time:"12:00  pm - 2:00 PM",
+    description:"Lorem ispm dolor sit ament , dirt ainitdngn"
+  },
+]
 function EventCalender() {
 
     const  [value,onChange]  = useState<Value>(new Date())
   return (
-    <div className="bg-white p-4 rounded-md"> <Calendar onChange={onChange} value={value} /></div>
+    <div className="bg-white p-4 rounded-md"> 
+    <Calendar onChange={onChange} value={value} />
+    {/* EVENTs */}
+    <div className="flex items-center justify-between">
+      <h1 className="text-xl font-semibold my-4">Events</h1>
+      <Image alt="" src="/moreDark.png" width={12} height={12} />
+    </div>
+    <div className="flex flex-col gap-4">
+
+    {events.map(event=>(
+      <div key={event.id} className="p-5 rounded-md border-2 border-gray-100 border-t-4 odd:border-t-lamaSky even:border-t-lamaPurple">
+        <div className="flex items-center justify-between">
+          <h1 className="font-semibold text-gray-600">{event.title}</h1>
+          <span className="text-gray-300 text-xs"> {event.time}</span>
+          </div>
+          <p className="mt-2 text-gray-400 text-sm">{event.description}</p>
+           </div>
+    ))}
+    </div>
+    </div>
   )
 }
 
