77cc3a7fe3208ec05690316985c51fc4e17b8ddc
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-03-13 14:18:18 +0500

    Barchart completed

--- src/components/AttendenceChart.jsx
+++ src/components/AttendenceChart.jsx
@@ -39,8 +39,8 @@ const data = [
 function AttendenceChart() {
   return (
     <div className="w-full bg-white rounded-lg p-4 h-full">
-        <div className="">
-            <h1>Attendece</h1>
+        <div className="flex justify-between items-center">
+            <h1 className="text-lg font-semibold">Attendece</h1>
             <Image src="/moreDark.png" width={20} height={20} alt=""/>
         </div>
         <ResponsiveContainer width="100%" height="100%">
@@ -48,17 +48,17 @@ function AttendenceChart() {
           width={500}
           height={300}
           data={data}
-       barSize={20}
+          barSize={20}
         >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
+          <CartesianGrid strokeDasharray="3 3" vertical={false}/>
+          <XAxis dataKey="name" axisLine={false} tick={{fill:"#d1d5db"}} tickLine={false}/>
+          <YAxis  axisLine={false} tickLine={false}/>
+          <Tooltip contentStyle={{borderRadius:'10px', borderColor:'lightgray'}} />
           <Legend align="left" verticalAlign="top" wrapperStyle={{padding:"20px",paddingBottom:"40px"}}/>
 
-          <Bar dataKey="present" fill="#FAE27C" activeBar={<Rectangle fill="pink" stroke="blue" />} legendType="circle"/>
+          <Bar dataKey="present" fill="#FAE27C"  legendType="circle" radius={[10,10,0,0]}/>
 
-          <Bar dataKey="absent" fill="#C3EBFA" activeBar={<Rectangle fill="gold" stroke="purple" />} legendType="circle" />
+          <Bar dataKey="absent" fill="#C3EBFA"  legendType="circle" radius={[10,10,0,0]} />
         </BarChart>
       </ResponsiveContainer>
     </div>
