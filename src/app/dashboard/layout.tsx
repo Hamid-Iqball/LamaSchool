bbb9634c8a5db7df90cb97c930db0524dd437819
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-03-15 11:49:09 +0500

    all the homepages completed

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -18,7 +18,7 @@ export default function DashboardLayout({children,}: Readonly<{children: React.R
     </div>
 
     {/* Right */}
-    <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-auto " >
+    <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-auto flex flex-col " >
       <Navbar/>
       {children}
     </div>
--- src/app/dashboard/teacher/page.tsx
+++ src/app/dashboard/teacher/page.tsx
@@ -1,8 +1,27 @@
 
 
+import Announcement from "@/components/Announcement"
+import BigCalander from "@/components/BigCalander"
+
+
+
 function TeacherPage() {
   return (
-    <div>page</div>
+    <div className="p-4 flex gap-4 flex-col xl:flex-row flex-1">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+      <div className="h-full bg-white p-4 rounded-md  ">
+        <h1 className="text-xl font-semibold ">Schedule</h1>
+        <BigCalander />
+      </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8 ">
+
+      
+      <Announcement />
+      </div>
+    </div>
   )
 }
 
