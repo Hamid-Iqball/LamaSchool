eac498062e80b373459cbb09d65f3cfbe5c95b9a
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-03-15 18:09:18 +0500

    Added sections

---
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -0,0 +1,114 @@
+import Paginations from "@/components/Paginations"
+import Table from "@/components/Table"
+import TableSearch from "@/components/TableSearch"
+import { parentsData, role, studentsData, teachersData } from "@/lib/data"
+import { headers } from "next/headers"
+import Image from "next/image"
+import Link from "next/link"
+
+type Parent ={
+  id:number;
+  name:string;
+  email?:string;
+  students:string[];
+  phone:string;
+  address:string;
+
+
+}
+const columns =[
+  {
+    header:"Info", accessor:"Info"
+
+  },
+  {
+    header:"Student Name", 
+    accessor:"studentName", 
+    className:"hidden md:table-cell"
+
+  },
+  {
+    header:"Phone", 
+    accessor:"phone", 
+    className:"hidden md:table-cell"
+
+  },
+  
+  {
+    header:"Address", 
+    accessor:"address", 
+    className:"hidden lg:table-cell"
+
+  },
+  {
+    header:"Actions",
+    accessor:"actions"
+  }
+]
+
+const renderRow = (item:Parent)=>(
+<tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight">
+  <td className="flex items-center gap-4 p-4">
+    
+
+    <div className="flex flex-col">
+      <h3 className="font-semibold">{item.name}</h3>
+      <p className="text-xs text-gray-500">{item?.email}</p>
+    </div>
+  </td>
+    <td className="hidden md:table-cell">{item.students.join("")}</td>
+
+    <td className="hidden md:table-cell">{item.phone}</td>
+    <td className="hidden md:table-cell">{item.address}</td>
+  <td>
+    <div className="flex items-center gap-2">
+      <Link href={`list/teachers/${item.id}`}>
+      <button className="w-7 h-7 flex justify-center items-center gap-2 bg-lamaSky rounded-full ">
+        <Image src="/edit.png" alt="" width={16} height={16}/>
+        </button>
+        </Link>
+        <Link href="">
+      { role==="admin" && <button className="w-7 h-7 flex justify-center items-center gap-2 bg-lamaPurple rounded-full ">
+        <Image src="/delete.png" alt="" width={16} height={16}/>
+        </button>}
+        </Link>
+        
+    </div>
+  </td>
+</tr>
+)
+
+function ParentsList() {
+  return (
+    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+      {/* Top */}
+    <div className="flex items-center justify-between">
+    <h1 className="hidden md:block text-lg font-semibold">All Parents</h1>
+    <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+      <TableSearch/>
+      <div className="flex items-center gap-4 self-end">
+        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+          <Image src="/filter.png" alt="" width={14} height={14} />
+        </button>
+        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+          <Image src="/sort.png" alt="" width={14} height={14} />
+        </button>
+       { role === "admin" && (<button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+          <Image src="/plus.png" alt="" width={14} height={14} />
+        </button> )}
+      </div>
+    </div>
+    </div>
+    {/* List */}
+    <div>
+      <Table columns={columns} renderRow={renderRow} data={parentsData}/>
+    </div>
+    {/* Pagination */}
+    <div className="">
+      <Paginations />
+    </div>
+    </div>
+  )
+}
+
+export default ParentsList
---
+++ src/app/(dashboard)/list/subjects/page.tsx
@@ -0,0 +1,100 @@
+import Paginations from "@/components/Paginations"
+import Table from "@/components/Table"
+import TableSearch from "@/components/TableSearch"
+import { parentsData, role, studentsData, subjectsData, teachersData } from "@/lib/data"
+
+import Image from "next/image"
+import Link from "next/link"
+
+type Subjects ={
+  id:number;
+  name:string;
+  teachers:string[];
+  address:string;
+
+
+}
+const columns =[
+  {
+    header:"Subject Name", accessor:"name"
+
+  },
+  {
+    header:"Teachers", 
+    accessor:"teachers", 
+    className:"hidden md:table-cell"
+
+  },
+
+  {
+    header:"Actions",
+    accessor:"actions"
+  }
+]
+
+const renderRow = (item:Subjects)=>(
+<tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight">
+  <td className="flex items-center gap-4 p-4">
+    
+
+ 
+      <h3 className="font-semibold">{item.name}</h3>
+     
+   
+  </td>
+    <td className="hidden md:table-cell">{item.teachers.join("")}</td>
+
+
+ 
+  <td>
+    <div className="flex items-center gap-2">
+      <Link href={`list/teachers/${item.id}`}>
+      <button className="w-7 h-7 flex justify-center items-center gap-2 bg-lamaSky rounded-full ">
+        <Image src="/edit.png" alt="" width={16} height={16}/>
+        </button>
+        </Link>
+        <Link href="">
+      { role==="admin" && <button className="w-7 h-7 flex justify-center items-center gap-2 bg-lamaPurple rounded-full ">
+        <Image src="/delete.png" alt="" width={16} height={16}/>
+        </button>}
+        </Link>
+        
+    </div>
+  </td>
+</tr>
+)
+
+function  SubjectsList() {
+  return (
+    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+      {/* Top */}
+    <div className="flex items-center justify-between">
+    <h1 className="hidden md:block text-lg font-semibold">All Parents</h1>
+    <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+      <TableSearch/>
+      <div className="flex items-center gap-4 self-end">
+        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+          <Image src="/filter.png" alt="" width={14} height={14} />
+        </button>
+        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+          <Image src="/sort.png" alt="" width={14} height={14} />
+        </button>
+       { role === "admin" && (<button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+          <Image src="/plus.png" alt="" width={14} height={14} />
+        </button> )}
+      </div>
+    </div>
+    </div>
+    {/* List */}
+    <div>
+      <Table columns={columns} renderRow={renderRow} data={subjectsData}/>
+    </div>
+    {/* Pagination */}
+    <div className="">
+      <Paginations />
+    </div>
+    </div>
+  )
+}
+
+export default SubjectsList
