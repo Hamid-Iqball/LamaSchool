64275a8bdc6d00a21101127048835467d516c968
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-03-15 10:21:31 +0500

    That's how we make the buttons clickable

--- src/components/BigCalander.tsx
+++ src/components/BigCalander.tsx
@@ -1,20 +1,28 @@
 "use client"
 
-import { Calendar, momentLocalizer } from 'react-big-calendar'
+import { Calendar, momentLocalizer, View, Views } from 'react-big-calendar'
 import moment from 'moment'
 import { calendarEvents } from '@/lib/data'
 import "react-big-calendar/lib/css/react-big-calendar.css"
+import { useState } from 'react'
 const localizer = momentLocalizer(moment)
 
 function BigCalander() {
+  const [view , setView] = useState<View>(Views.WORK_WEEK)
+  function handleView(selected:View){
+    setView(selected)
+  }
   return (
-    <div><Calendar
+    <Calendar
     localizer={localizer}
     events={calendarEvents}
     startAccessor="start"
     endAccessor="end"
+    views={["work_week","day"]}
+    view={view}
+    onView={handleView}
     style={{ height: 500 }}
-  /></div>
+  />
   )
 }
 
