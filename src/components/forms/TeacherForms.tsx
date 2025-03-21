ed00374104a3f06cd9a84a7b63a52baa19cc4b9c
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-03-21 11:37:52 +0500

    completed the teacher form

--- src/components/forms/TeacherForms.tsx
+++ src/components/forms/TeacherForms.tsx
@@ -3,8 +3,9 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Form, useForm } from "react-hook-form";
-import { z } from "zod";
+import { Schema, z } from "zod";
 import InputField from "../InputField";
+import Image from "next/image";
 
 const schema = z.object({
     username: z.string()
@@ -14,13 +15,17 @@ const schema = z.object({
     password:z.string().min(8, {message:"Password must be at least 8 characters long !"}),
     firstName:z.string().min(1, {message:"First Name is required!"}),
     lastName:z.string().min(1, {message:"Last Name is required!"}),
-    phonenumber:z.number().min(1, {message:"phone number is required!"}),
+    phone:z.number().min(1, {message:"phone number is required!"}),
     address:z.string().min(1, {message:"Address is required!"}),
+    bloodtype:z.string().min(1, {message:"bloodtype is required!"}),
     birthday:z.date({message:"Birthday is required!"}),
     sex:z.enum(["male", "female"], {message:"Sex in required"}),
     img:z.instanceof(File, {message:"Image is required!"})
    
   });
+
+
+  type Inputs = z.infer<typeof schema>
 
 function TeacherForms({type ,data}:{type:"create" | "update", data?:any}) {
 
@@ -28,25 +33,134 @@ function TeacherForms({type ,data}:{type:"create" | "update", data?:any}) {
         register,
         handleSubmit,
         formState: { errors },
-      } = useForm({
+      } = useForm<Inputs>({
         resolver: zodResolver(schema),
       });
 
-      const onSubmit = handleSubmit((data)=>{
+   const onSubmit = handleSubmit((data)=>{
         console.log(data)
       })
   return (
+
+
     <form className="flex flex-col gap-8" onSubmit={onSubmit}>
        <h1 className="text-xl font-semibold ">Create a new teacher</h1>
        <span className="text-xs text-gray-400 font-medium">Authentication Information</span>
       
-       <InputField  label="Username" name="Username" type="text" defaultValue={data?.username} register={register} error={errors.username}/>
+      <div className="flex flex-wrap justify-between gap-4">
+        
+       <InputField 
+        label="Username"
+        name="Username" 
+        
+        defaultValue={data?.username} 
+        register={register} 
+        error={errors?.username}/>
+
+       <InputField 
+        label="Email"
+        name="email" 
+        type="email" 
+        defaultValue={data?.email} 
+        register={register} 
+        error={errors?.email}/>
+  
+       <InputField 
+        label="Passwords"
+        name="password" 
+        type="password" 
+        defaultValue={data?.password} 
+        register={register} 
+        error={errors?.password}/>
+  
 
 
-       
+
+        </div>
+
 
        <span className="text-xs text-gray-400 font-medium">Personal Information </span>
+
+       <div className="flex justify-between flex-wrap gap-4">
+
+       <InputField 
+        label="First Name"
+        name="firstname" 
+        defaultValue={data?.firstName} 
+        register={register} 
+        error={errors?.firstName}/>
+
+       <InputField 
+        label="Last Name"
+        name="lastName" 
+        defaultValue={data?.lastName} 
+        register={register} 
+        error={errors?.lastName}/>
+
+       <InputField 
+        label="Phone"
+        name="phone" 
+        defaultValue={data?.phone} 
+        register={register} 
+        error={errors?.phone}/>
+
+       <InputField 
+        label="Address"
+        name="address" 
+        defaultValue={data?.address} 
+        register={register} 
+        error={errors?.address}/>
+
+       <InputField 
+        label="Blood Type"
+        name="bloodtype" 
+        
+        defaultValue={data?.bloodtype} 
+        register={register} 
+        error={errors?.bloodtype}/>
+
+
+       <InputField 
+        label="Birthday"
+        name="birthday" 
+        type="date" 
+        defaultValue={data?.birthday} 
+        register={register} 
+        error={errors?.birthday}
+        />
+
+
+        <div className="flex flex-col gap-2 w-full sm:w-1/4 ">
+        <label className="text-xs text-gray-400">Sex</label>
+        <select   className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm  w-full" {...register("sex")} defaultValue={data?.sex}> 
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+        {errors.sex?.message && <p className="text-xs text-red-400">{errors.sex?.message.toString()}</p>}
+
+        </div>
+
+
+        <div className="flex flex-col gap-2 w-full sm:w-1/4 justify-center">
+
+        <label className="text-xs text-gray-400 flex items-center gap-2 cursor-pointer" htmlFor="img">
+          <Image alt="upload icon"
+          src="/upload.png"
+          width={28} height={28}/>
+          <span>Upload a photo</span>
+        </label>
+        < input type="file" {...register("img")} className="hidden" id="img"/>
        
+        
+        {errors.img?.message && <p className="text-xs text-red-400">{errors.img?.message.toString()}</p>}
+
+        </div>
+
+
+        </div>
+
+
+
        <button className="bg-blue-400 text-white p-2 rounded-md">{type==="create" ?"Create" : "Update"}</button>
     </form>
   )
