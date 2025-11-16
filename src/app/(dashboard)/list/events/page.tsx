bb180e68a13eaa8f629b7537800b297cb17ee90a
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-11-16 12:12:26 +0500

    fetched events data

--- src/app/(dashboard)/list/events/page.tsx
+++ src/app/(dashboard)/list/events/page.tsx
@@ -3,21 +3,13 @@ import Paginations from "@/components/Paginations"
 import Table from "@/components/Table"
 import TableSearch from "@/components/TableSearch"
 import { eventsData, examsData, parentsData, resultsData, role, studentsData, subjectsData, teachersData } from "@/lib/data"
-
+import prisma from "@/lib/prisma"
+import { ITEMS_PER_PAGE } from "@/lib/settings"
+import { Class, Event, Prisma } from "@prisma/client"
 import Image from "next/image"
-import Link from "next/link"
-
-type Event ={
-  id:number;
-  title:string;
-  class:number;
-  date:string;
-  startTime:string;
-  endTime:string
-  
 
 
-}
+type EventList = Event &{class:Class}
 const columns =[
   {
     header:"Title",
@@ -56,16 +48,24 @@ const columns =[
   }
 ]
 
-const renderRow = (item:Event)=>(
+const renderRow = (item:EventList)=>(
 <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight">
   
   <td className="flex items-center gap-4 p-4">
    {item.title}
      </td>
-    <td >{item.class}</td>
-    <td className="hidden md:table-cell">{item.date}</td>
-    <td className="hidden md:table-cell">{item.startTime}</td>
-    <td  className="hidden md:table-cell">{item.endTime}</td>
+    <td >{item.class.name}</td>
+    <td className="hidden md:table-cell">{new Intl.DateTimeFormat("en-US").format(item.startTime)}</td>
+    <td className="hidden md:table-cell">{item.startTime.toLocaleString("en-US", {
+      hour:"2-digit",
+      minute:"2-digit",
+      hour12:false
+    })}</td>
+    <td  className="hidden md:table-cell">{item.endTime.toLocaleString("en-US", {
+      hour:"2-digit",
+      minute:"2-digit",
+      hour12:false
+    })}</td>
     
    
 
@@ -84,7 +84,51 @@ const renderRow = (item:Event)=>(
 </tr>
 )
 
-function  EventListPage() {
+async function EventListPage({searchParams}:{
+  searchParams: {[key:string]: string | undefined}
+}) {
+
+
+
+  const {page ,...queryParams} = searchParams
+  const p = page? parseInt(page) : 1
+  
+  const query: Prisma.EventWhereInput = {}
+  //this logic is for the query
+      if(queryParams){
+        for(const [key,value] of Object.entries(queryParams)){
+          if(value !== undefined){
+            switch (key) {
+               case "search":
+       query.title = {contains:value, mode:"insensitive"} 
+          break
+            
+              default:
+                break;
+            }
+        
+              }
+            }
+          }
+        
+    
+
+        // This is prisma interaction function
+    const [data,count] = await prisma.$transaction([
+      
+      //remmeber prisma doesnot fetch relations automatically we have to mention it in the query
+      prisma.event.findMany({
+        where:query,
+        include:{
+         class: true
+        },
+        take:ITEMS_PER_PAGE,
+        skip: ITEMS_PER_PAGE*(p-1),
+      }),
+      
+      prisma.event.count({where:query})
+      
+    ])
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* Top */}
@@ -105,11 +149,11 @@ function  EventListPage() {
     </div>
     {/* List */}
     <div>
-      <Table columns={columns} renderRow={renderRow} data={eventsData}/>
+      <Table columns={columns} renderRow={renderRow} data={data}/>
     </div>
     {/* Pagination */}
     <div className="">
-      <Paginations />
+      <Paginations page={p}  count={count} />
     </div>
     </div>
   )
