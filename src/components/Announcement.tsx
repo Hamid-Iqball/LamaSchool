be6bf9d22dabbb788178d2cb55f92d62e7ed40cf
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-11-26 12:51:30 +0500

    finished the Admin page completely sync it with database

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -1,39 +1,88 @@
-"use client"
+import prisma from "@/lib/prisma"
+import { auth } from "@clerk/nextjs/server"
+import Link from "next/link"
 
-function Announcement() {
+async function Announcement() {
+  const {userId, sessionClaims} = await auth()
+  const role = (sessionClaims?.metadata as {role:string})?.role
 
+  const roleCondition = {
+
+    teacher: { 
+      lessons: { 
+        some: { 
+          teacherId: userId! 
+        } 
+      } 
+    },
+    student: { 
+      students: { 
+        some: { 
+          id: userId! 
+        } 
+      } 
+    },
+    parent: { 
+      students: { 
+        some: { 
+          parentId: userId! 
+        } 
+      } 
+    }
+  }
+
+  const data = await prisma.announcement.findMany({
+    take: 3,
+    orderBy: { date: "desc" },
+    where: {
+     ...(role!=="admin"&&{ OR: [
+        { classId: null }, // Announcements for all classes
+        { class: roleCondition[role as keyof typeof roleCondition] || {} }
+      ]})
+    }
+  })
+
+  console.log("announcements", data)
 
   return (
     <div className="bg-white p-4 rounded-md">
-        <div className="flex items-center justify-between">
-            <h1 className="text-lg font-semibold">Announcement</h1>
-            <span className="text-xs text-gray-400">View All</span>
-        </div>
-    <div className="flex flex-col gap-4 mt-4">
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-semibold">Announcement</h1>
+        <Link href="/" className="text-xs text-gray-400">View All</Link>
+      </div>
+{ data ?     <div className="flex flex-col gap-4 mt-4">
         <div className="bg-lamaSkyLight rounded-md p-4">
-            <div className="flex items-center justify-between">
-                <h2>Lorem ipsum dolor sit amet.</h2>
-                <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">2025-01-01 </span>
-            </div>
-            <p  className="text-gray-400 text-xs mt-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat rerum et consequuntur veritatis</p>
+          <div className="flex items-center justify-between">
+            <h2 className="font-medium">{data[0]?.title}</h2>
+            <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
+              {data[0]?.date.toLocaleDateString('en-US')}
+            </span>
+          </div>
+          <p className="text-sm text-gray-400 mt-1">{data[0]?.description}</p>
         </div>
-
-        <div className="bg-lamaPurpleLight rounded-md p-4">
-            <div className="flex items-center justify-between">
-                <h2>Lorem ipsum dolor sit amet.</h2>
-                <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">2025-01-01 </span>
-            </div>
-            <p className="text-gray-400 text-xs mt-1" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat rerum et consequuntur veritatis</p>
+        <div className="bg-purple-100 rounded-md p-4">
+          <div className="flex items-center justify-between">
+            <h2 className="font-medium">{data[1]?.title}</h2>
+            <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
+              {data[1]?.date.toLocaleDateString('en-US')}
+            </span>
+          </div>
+          <p className="text-sm text-gray-400 mt-1">{data[1]?.description}</p>
         </div>
-
-        <div className="bg-lamaYellowLight rounded-md p-4">
-            <div className="flex items-center justify-between">
-                <h2>Lorem ipsum dolor sit amet.</h2>
-                <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">2025-01-01 </span>
-            </div>
-            <p  className="text-gray-400 text-xs mt-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat rerum et consequuntur veritatis</p>
+        <div className="bg-lamaSkyLight rounded-md p-4">
+          <div className="flex items-center justify-between">
+            <h2 className="font-medium">{data[2]?.title}</h2>
+            <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
+              {data[2]?.date.toLocaleDateString('en-US')}
+            </span>
+          </div>
+          <p className="text-sm text-gray-400 mt-1">{data[2]?.description}</p>
         </div>
-    </div>
+      </div>:
+      <div className="flex justify-center items-center">
+        <p className="text-xl text-red-300">No Data to be shown</p>
+      </div>
+      }
     </div>
   )
 }
